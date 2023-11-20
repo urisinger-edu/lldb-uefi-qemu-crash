f3e29d9958c779a2f579c88797b970a0131d47be
username <emailaddress@yahoo.com>
2023-11-20 13:58:16 +0200

    first commit

---
+++ build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const Feature = @import("std").Target.Cpu.Feature;
+
+pub fn build(b: *std.Build) void {
+    var target = b.standardTargetOptions(.{
+        .default_target = .{
+            .os_tag = .uefi,
+            .cpu_arch = .x86_64,
+            .abi = .msvc,
+        },
+    });
+
+    b.exe_dir = "zig-out/EFI/BOOT/";
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "bootx64",
+        .root_source_file = std.Build.FileSource.relative("src/main.zig"),
+        .target = target,
+        .optimize = mode,
+    });
+
+    b.default_step.dependOn(&exe.step);
+
+    const install_step = b.addInstallArtifact(exe, .{});
+
+    const run_cmd = b.addSystemCommand(&.{ "qemu-system-x86_64", "-vga", "virtio", "--no-reboot", "--no-shutdown", "-bios", "OVMF.fd", "-drive", "format=raw,file=fat:rw:zig-out" });
+    run_cmd.step.dependOn(&install_step.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+const uefi = std.os.uefi;
+
+fn uefi_write(context: void, bytes: []const u8) error{}!usize {
+    _ = context;
+
+    for (bytes) |byte| {
+        // Convert UTF-8 to UTF-16 and output each character
+        var char = [_:0]u16{ @as(u16, byte), 0 };
+        _ = uefi.system_table.con_out.?.outputString(&char);
+    }
+
+    return bytes.len;
+}
+
+const UefiWriter = std.io.Writer(void, error{}, uefi_write);
+
+const stdout: UefiWriter = .{ .context = undefined };
+
+pub fn main() void {
+    var loaded_image: *uefi.protocols.LoadedImageProtocol = undefined;
+    const guid: uefi.Guid = uefi.protocols.LoadedImageProtocol.guid;
+    _ = uefi.system_table.boot_services.?.handleProtocol(uefi.handle, @alignCast(&guid), @ptrCast(&loaded_image));
+
+    stdout.print("Image base: 0x{x}\n", .{@intFromPtr(loaded_image.*.image_base)}) catch unreachable;
+
+    var wait = true;
+    while (wait) {
+        asm volatile ("pause");
+    }
+}
